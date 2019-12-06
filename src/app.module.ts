370fe75aa2cbb2540b6ccdf2b77e234577da4bb4
Deary <dearyherdiano@gmail.com>
2019-12-06 18:22:49 +0800

    first initialize

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ItemsController } from './items/items.controller';
+import { ItemsService } from './items/items.service';
+import { ItemsModule } from './items/items.module';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from './config/keys';
+
+@Module({
+  imports: [ItemsModule, UsersModule, MongooseModule.forRoot(config.mongoURI)],
+  controllers: [AppController, ItemsController, UsersController],
+  providers: [AppService, ItemsService, UsersService],
+})
+export class AppModule {}
---
+++ src/config/keys.ts
@@ -0,0 +1,3 @@
+export default {
+  mongoURI: 'mongodb+srv://ketibansapi:dg6x2wJsBYC8bu5@cluster0-bxhsm.mongodb.net/test?retryWrites=true&w=majority',
+};
